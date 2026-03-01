40f0473464a29a77314583b998dced2298832852
sergey <serjjiniuss@gmail.com>
2026-03-01 15:34:28 +0400

    User+Validation

--- Back/app.js
+++ Back/app.js
@@ -5,14 +5,46 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+
+    filename: (req, file, cb) => {
+        cb(
+            null,
+            new Date().toISOString().replace(/:/g, "-") +
+                "-" +
+                file.originalname,
+        );
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 app.use(bodyParser.json());
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"),
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
@@ -29,19 +61,21 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
-(app.use((error, req, res, next) => {
+app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
+    const data = error.data;
 
-    res.status(status).json({ message: message });
-}),
-    mongoose
-        .connect(MONGODB_URI)
-        .then((result) => {
-            app.listen(8080);
-        })
-        .catch((err) => {
-            console.log(err);
-        }));
+    res.status(status).json({ message: message, data: data });
+});
+mongoose
+    .connect(MONGODB_URI)
+    .then((result) => {
+        app.listen(8080);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
