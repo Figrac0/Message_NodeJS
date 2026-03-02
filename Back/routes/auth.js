96cc42c883c0e45a983689d3c6c5da95e63dd0c9
sergey <serjjiniuss@gmail.com>
2026-03-02 22:12:29 +0400

    Add auth

--- Back/routes/auth.js
+++ Back/routes/auth.js
@@ -12,17 +12,20 @@ router.put(
         body("email")
             .isEmail()
             .withMessage("Please enter a valid email.")
-            .custom((value, { req }) => {
+            .custom((value) => {
                 return User.findOne({ email: value }).then((userDoc) => {
                     if (userDoc) {
                         return Promise.reject("E-Mail address already exists!");
                     }
                 });
-            }).normalizeEmail,
+            })
+            .normalizeEmail(),
         body("password").trim().isLength({ min: 5 }),
         body("name").trim().not().isEmpty(),
     ],
     authController.signup,
 );
+
+router.post("/login", authController.login);
 
 module.exports = router;
--- Back/routes/feed.js
+++ Back/routes/feed.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const { body } = require("express-validator");
 
+const isAuth = require("../middleware/is-auth");
+
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 router.post(
     "/post",
+    isAuth,
     [
         body("title").trim().isLength({ min: 5 }),
         body("content").trim().isLength({ min: 5 }),
@@ -16,10 +19,11 @@ router.post(
     feedController.createPost,
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 
 router.put(
     "/post/:postId",
+    isAuth,
     [
         body("title").trim().isLength({ min: 5 }),
         body("content").trim().isLength({ min: 5 }),
@@ -27,6 +31,6 @@ router.put(
     feedController.updatedPosts,
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
